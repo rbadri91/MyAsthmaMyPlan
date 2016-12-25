5e2fadc15fb023d6234ab27e82bcf1b3a6ae4613
badri <rbadri01@gmail.com>
2016-12-25 18:15:38 -0500

    some changes in doctor

--- config/routes.js
+++ config/routes.js
@@ -45,6 +45,7 @@ module.exports = function(app, passport, user, fs) {
  			output.data.pending_patient_requests.pull(patient_email_id);
  			output.save();
  		});
+ 	res.send("Added Successfully");
  });
  app.post('/removeFromPatientList', function(req, res) {
  	console.log("removeFromPatientList req user obj is ", req.body);
