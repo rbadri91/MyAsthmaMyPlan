9667509f9483ec0dcb7e5681cfafc6cc95f03185
Sanket Dige <sanketdige268@gmail.com>
2016-12-25 19:02:28 -0500

    changes for request handling

--- config/routes.js
+++ config/routes.js
@@ -8,75 +8,80 @@ module.exports = function(app, passport, user, fs) {
 	app.get('/login', alreadyLoggedIn, function(req,res) {
 		res.render('login',{title : 'Login', message: req.flash('loginMessage')});
 	});
- app.post('/login', passport.authenticate('local-login',{
+	app.post('/login', passport.authenticate('local-login',{
         failureRedirect : '/login', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }),
- function(req,res){
- 	if(req.user.data.role=="Doctor")
- 		res.redirect('/doctor');
- 	else
- 		res.redirect('/home');
- });
- app.get('/signup', alreadyLoggedIn, function(req, res) {
- 	res.render('signup', {title: 'Sign Up', message: req.flash('signupMessage')});
- });
- app.post('/signup',passport.authenticate('local-signup',{
+	function(req,res){
+		if(req.user.data.role=="Doctor")
+			res.redirect('/doctor');
+		else
+			res.redirect('/home');
+	});
+	app.get('/signup', alreadyLoggedIn, function(req, res) {
+		res.render('signup', {title: 'Sign Up', message: req.flash('signupMessage')});
+	});
+	app.post('/signup',passport.authenticate('local-signup',{
         failureRedirect : '/signup', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }),
- function(req,res){
- 	console.log("user data:",req.user.data);
- 	if(req.user.data.role=="Doctor")
- 		res.redirect('/doctor');
- 	else
- 		res.redirect('/home');
- });
- app.post('/addToPatientList', function(req, res) {
- 	console.log("addToPatientList req user obj is ", req.body);
- 	console.log("addToPatientList req body obj is ", req.user);
+	function(req,res){
+		console.log("user data:",req.user.data);
+		if(req.user.data.role=="Doctor")
+			res.redirect('/doctor');
+		else
+			res.redirect('/home');
+	});
+	app.post('/addToPatientList', function(req, res) {
+		console.log("addToPatientList req user obj is ", req.body);
+		console.log("addToPatientList req body obj is ", req.user);
 
- 	var patient_email_id = req.body.patientEmail;
- 	Doctor.findOne({
- 		'data.email': req.user.data.email, 'data.patient_list' : patient_email_id}, 
- 		function(err, output, res) {
- 			console.log("Inside Doctor findone ", output);
- 			if(err) return err;
- 			//output.data.find({patient_list : patient_email_id}), function(req, data) {
- 				// if(err)
- 				// 	res.send(err);
- 				if(output){
- 					output.data.pending_patient_requests.pull(patient_email_id);
- 					output.save();
- 					return res;
- 				}
- 				output.data.patient_list.push(patient_email_id);
-	 			output.data.pending_patient_requests.pull(patient_email_id);
-	 			output.save();
-	 			return res;
- 			//};	
- 		});
- });
- app.post('/removeFromPendingPatientRequests', function(req, res) {
- 	console.log("removeFromPatientList req user obj is ", req.body);
- 	console.log("removeFromPatientList req body obj is ", req.user);
+		var patient_email_id = req.body.patientEmail;
+		Doctor.findOne({
+			'data.email': req.user.data.email, 'data.patient_list' : patient_email_id}, 
+			function(err, output) {
+				console.log("Inside Doctor findone ", output);
+				if(err){
+					res.send(err);
+					return;	
+				} 
 
- 	var patient_email_id = req.body.patientEmail;
- 	Doctor.findOne({
- 		'data.email': req.user.data.email}, 
- 		function(err, output) {
- 			if(err) return err;
- 			output.data.pending_patient_requests.pull(patient_email_id);
- 			output.save();
- 		});
- });
+				if(output){
+					output.data.pending_patient_requests.pull(patient_email_id);
+					output.save();
+					res.send("Patient already exists in Patient List");
+					return;
+				}
+				output.data.patient_list.push(patient_email_id);
+				output.data.pending_patient_requests.pull(patient_email_id);
+				output.save();
+				res.send("Patient successfully added to Patient List");
+			});
+	});
+	app.post('/removeFromPendingPatientRequests', function(req, res) {
+		console.log("removeFromPatientList req user obj is ", req.body);
+		console.log("removeFromPatientList req body obj is ", req.user);
 
- app.post('/doctorRequest', function(req, res) {
- 	console.log("email is ", req.body.email);
- 	var user_email_id = req.user.data.email;
- 	console.log("user emailid ", user_email_id);
- 	Doctor.findOne({'data.email':req.body.email},function(err, output) {
- 			if(err) return err;
+		var patient_email_id = req.body.patientEmail;
+		Doctor.findOne({
+			'data.email': req.user.data.email}, 
+			function(err, output) {
+				if(err) {
+					res.send(err);
+					return;
+				} 
+				output.data.pending_patient_requests.pull(patient_email_id);
+				output.save();
+				res.send("Patient request successfully removed from Pending Patient Requests")
+			});
+	});
+
+	app.post('/doctorRequest', function(req, res) {
+		console.log("email is ", req.body.email);
+		var user_email_id = req.user.data.email;
+		console.log("user emailid ", user_email_id);
+		Doctor.findOne({'data.email':req.body.email},function(err, output) {
+			if(err) return err;
  			//patientList = output.data.patient_list;
  			output.data.pending_patient_requests.push(user_email_id);
  			output.save();
@@ -86,45 +91,45 @@ module.exports = function(app, passport, user, fs) {
  	res.redirect('/home');
  });
 
- app.get('/doctor', isLoggedIn,function(req, res) {
- 		var patientList,pendingPatientList;
- 		Doctor.findOne({'data.email':req.user.data.email},function(err, output) {
- 			if(err) return err;
- 			pending_patient_Requests = output.data.pending_patient_requests;
- 			console.log("pending_patient_Requests in routes:",pending_patient_Requests);
- 			patientList = output.data.patient_list;
- 			res.render('doctor', {title: 'doctor', usr: req.user,pList: patientList,pendingPList:pending_patient_Requests});
+	app.get('/doctor', isLoggedIn,function(req, res) {
+		var patientList,pendingPatientList;
+		Doctor.findOne({'data.email':req.user.data.email},function(err, output) {
+			if(err) return err;
+			pending_patient_Requests = output.data.pending_patient_requests;
+			console.log("pending_patient_Requests in routes:",pending_patient_Requests);
+			patientList = output.data.patient_list;
+			res.render('doctor', {title: 'doctor', usr: req.user,pList: patientList,pendingPList:pending_patient_Requests});
 			loaded = true;
- 		});
+		});
 		
 	});
- app.get('/home', isLoggedIn,function(req, res) {
+	app.get('/home', isLoggedIn,function(req, res) {
 		res.render('home', {title: 'Home', usr: req.user});
 		loaded = true;
 	});
- app.get('/fileupload', isLoggedIn, function(req, res) {
- 	res.render('fileupload', {title: 'FileUpload', usr: req.user});
- 	loaded = true;
- });
+	app.get('/fileupload', isLoggedIn, function(req, res) {
+		res.render('fileupload', {title: 'FileUpload', usr: req.user});
+		loaded = true;
+	});
 
- app.get('/profile', isLoggedIn, function(req, res) {
- 	res.render('profile', {title: 'Profile', usr: req.user});
- 	loaded = true;
- });
- app.get('/viewPatientProfile', isLoggedIn, function(req, res) {
- 	res.render('viewPatientProfile', {title: 'viewPatientProfile', usr: req.user});
- 	loaded = true;
- });
- app.get('/mamp', isLoggedIn, function(req, res) {
+	app.get('/profile', isLoggedIn, function(req, res) {
+		res.render('profile', {title: 'Profile', usr: req.user});
+		loaded = true;
+	});
+	app.get('/viewPatientProfile', isLoggedIn, function(req, res) {
+		res.render('viewPatientProfile', {title: 'viewPatientProfile', usr: req.user});
+		loaded = true;
+	});
+	app.get('/mamp', isLoggedIn, function(req, res) {
 
- 	res.render('mamp', {title: 'MyAsthmaMyPlan', usr: req.user});
- 	loaded = true;
- });
- app.get('/logout', function(req, res) {
- 	req.logout();
- 	res.redirect('/');
- 	loaded = false;
- });
+		res.render('mamp', {title: 'MyAsthmaMyPlan', usr: req.user});
+		loaded = true;
+	});
+	app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+		loaded = false;
+	});
 
 //};
 app.get('/send', function(req,res) {
