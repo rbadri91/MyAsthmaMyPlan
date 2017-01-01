3e6af6dcb330983c3719106f148db44c6f11b11b
badri <rbadri01@gmail.com>
2017-01-01 03:49:07 -0500

    Changes for dropdown selection

--- config/routes.js
+++ config/routes.js
@@ -247,7 +247,7 @@ module.exports = function(app, passport, fs, MAMP_files_path) {
                 	var myPlans = output.data.myPlans;
                 	console.log(myPlans);
                 	// res.send("get patient history call was successful.");
-                	res.render('MyPlanHistory', {title: 'My Plan History', usr: req.user,usrDetails:userDetails});
+                	res.render('MyPlanHistory', {title: 'My Plan History', usr: req.user,usrDetails:userDetails,fileDetails:myPlans});
 					return;
 				}
 			});
